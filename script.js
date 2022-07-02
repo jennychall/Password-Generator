e64eea6de0f810a4b0556f9b7886957e1ea45030
Jenny Hall <jenny hall>
2022-07-01 18:41:50 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,111 @@
+// Assignment code here
+var specialCharacters = [
+  "@",
+  "%",
+  "+",
+  "\\",
+  "/",
+  "'",
+  "!",
+  "#",
+  "$",
+  "^",
+  "?",
+  ":",
+  ",",
+  ")",
+  "(",
+  "}",
+  "{",
+  "]",
+  "[",
+  "~",
+  "-",
+  "_",
+  ".",
+
+];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+//Array, lowercase characters// 
+var lowerCase = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+]; 
+
+//Array for uppercase letters//
+var upperCase = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O", 
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "X",
+  "Y",
+  "Z",
+]; 
+
+function generatePassword(){
+  var passwordLength = prompt ("What length of password would you like?"); 
+  var hasSpecialChar = confirm ("Include Special Characters");
+  var hasUpperChar = confirm ("Include UpperCase Characters");
+  var hasLowerChar = confirm ("Include LowerCase Characters");
+  var hasNumbers = confirm ("Include Numbers");
+  var hasSpecialChar = confirm ("Include Special Characters");
+}
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
